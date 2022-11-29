c69303fcfcbae7a75386fb059338f58207abb474
nkr22 <81866898+nkr22@users.noreply.github.com>
2022-11-28 17:29:17 -0700

    fix starter

--- y-work/src/views/Starter.js
+++ y-work/src/views/Starter.js
@@ -43,58 +43,89 @@ class Starter extends React.Component {
             });
     }
 
-  render() {
-    const { isLoaded, items } = this.state;
-    
-    if (!isLoaded) {
-      return <div>Loading...</div>;
-    } else {
-      return (
-        <div>
-          <Row>
-            {items.map((item) => (
-              <Col md="12" lg="6">
-                <Card body className="text-end">
-                  <CardTitle tag="h5" className="border-bottom">
-                    {item.posting_title}
-                  </CardTitle>
-                  <CardText key={item.opening_id} className="">
-                    <p>
-                      <span style={{ fontWeight: "bold", float: "left" }}>
-                        Wage:
-                      </span>
-                      ${item.min_salary}-${item.max_salary}
-                    </p>
-                    <p>
-                      <span style={{ fontWeight: "bold", float: "left" }}>
-                        Department:
-                      </span>
-                      {item.department}
-                    </p>
-                    <p>
-                      <span style={{ fontWeight: "bold", float: "left" }}>
-                        Category:
-                      </span>{" "}
-                      {item.category_name}
-                    </p>
-                    <p>
-                      <span style={{ fontWeight: "bold", float: "left" }}>
-                        Start Date:
-                      </span>{" "}
-                      {item.start_date}
-                    </p>
-                  </CardText>
-                  <div>
-                    <Link to="/apply" className="btn btn-info">
-                      Apply
-                    </Link>
-                  </div>
-                </Card>
-              </Col>
-            ))}
-          </Row>
-        </div>
-      );
+    render() {
+        const { isLoaded, items } = this.state;
+
+        if (!isLoaded) {
+            return <div>Loading...</div>;
+        } else {
+            return (
+                <div>
+                    <Row>
+                        {items.map((item) => (
+                            <Col md="12" lg="6">
+                                <Card body className="text-end">
+                                    <CardTitle
+                                        tag="h5"
+                                        className="border-bottom"
+                                    >
+                                        {item.posting_title}
+                                    </CardTitle>
+                                    <CardText
+                                        key={item.opening_id}
+                                        className=""
+                                    >
+                                        <p>
+                                            <span
+                                                style={{
+                                                    fontWeight: "bold",
+                                                    float: "left",
+                                                }}
+                                            >
+                                                Wage:
+                                            </span>
+                                            ${item.min_salary}-$
+                                            {item.max_salary}
+                                        </p>
+                                        <p>
+                                            <span
+                                                style={{
+                                                    fontWeight: "bold",
+                                                    float: "left",
+                                                }}
+                                            >
+                                                Department:
+                                            </span>
+                                            {item.department}
+                                        </p>
+                                        <p>
+                                            <span
+                                                style={{
+                                                    fontWeight: "bold",
+                                                    float: "left",
+                                                }}
+                                            >
+                                                Category:
+                                            </span>{" "}
+                                            {item.category_name}
+                                        </p>
+                                        <p>
+                                            <span
+                                                style={{
+                                                    fontWeight: "bold",
+                                                    float: "left",
+                                                }}
+                                            >
+                                                Start Date:
+                                            </span>{" "}
+                                            {item.start_date}
+                                        </p>
+                                    </CardText>
+                                    <div>
+                                        <Link
+                                            to="/apply"
+                                            className="btn btn-info"
+                                        >
+                                            Apply
+                                        </Link>
+                                    </div>
+                                </Card>
+                            </Col>
+                        ))}
+                    </Row>
+                </div>
+            );
+        }
     }
 }
 
