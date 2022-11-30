c7541231a99902c20c7fad332eb5f6982dda6f55
nkr22 <81866898+nkr22@users.noreply.github.com>
2022-11-29 20:17:23 -0700

    stars on page and breadcrumbs on apply page

--- y-work/src/layouts/Header.js
+++ y-work/src/layouts/Header.js
@@ -1,79 +1,83 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import {
-  Navbar,
-  Collapse,
-  Nav,
-  NavItem,
-  NavbarBrand,
-  UncontrolledDropdown,
-  DropdownToggle,
-  DropdownMenu,
-  DropdownItem,
-  Dropdown,
-  Button,
+    Navbar,
+    Collapse,
+    Nav,
+    NavItem,
+    NavbarBrand,
+    UncontrolledDropdown,
+    DropdownToggle,
+    DropdownMenu,
+    DropdownItem,
+    Dropdown,
+    Button,
 } from "reactstrap";
 import Logo from "./Logo";
 import { ReactComponent as LogoWhite } from "../assets/images/logos/materialpro.svg";
 import user5 from "../assets/images/users/user5.jpg";
 
 const Header = () => {
-  const [isOpen, setIsOpen] = React.useState(false);
+    const [isOpen, setIsOpen] = React.useState(false);
 
-  const [dropdownOpen, setDropdownOpen] = React.useState(false);
+    const [dropdownOpen, setDropdownOpen] = React.useState(false);
 
-  const toggle = () => setDropdownOpen((prevState) => !prevState);
-  const Handletoggle = () => {
-    setIsOpen(!isOpen);
-  };
-  const showMobilemenu = () => {
-    document.getElementById("sidebarArea").classList.toggle("showSidebar");
-  };
-  return (
-    <Navbar color="primary" dark expand="md" className="fix-header">
-      <div className="d-flex align-items-center">
-        <div className="d-lg-block d-none me-5 pe-3">
-          <Logo />
-        </div>
-        <NavbarBrand href="/">
-          <LogoWhite className=" d-lg-none" />
-        </NavbarBrand>
-        <Button
-          color="primary"
-          className=" d-lg-none"
-          onClick={() => showMobilemenu()}
-        >
-          <i className="bi bi-list"></i>
-        </Button>
-      </div>
-      <div className="hstack gap-2">
-        <Button
-          color="primary"
-          size="sm"
-          className="d-sm-block d-md-none"
-          onClick={Handletoggle}
-        >
-          {isOpen ? (
-            <i className="bi bi-x"></i>
-          ) : (
-            <i className="bi bi-three-dots-vertical"></i>
-          )}
-        </Button>
-      </div>
+    const toggle = () => setDropdownOpen((prevState) => !prevState);
+    const Handletoggle = () => {
+        setIsOpen(!isOpen);
+    };
+    const showMobilemenu = () => {
+        document.getElementById("sidebarArea").classList.toggle("showSidebar");
+    };
+    return (
+        <Navbar color="primary" dark expand="md" className="fix-header">
+            <div className="d-flex align-items-center">
+                <div className="d-lg-block d-none me-5 pe-3">
+                    <Logo />
+                </div>
+                <NavbarBrand href="/">
+                    <LogoWhite className=" d-lg-none" />
+                </NavbarBrand>
+                <Button
+                    color="primary"
+                    className=" d-lg-none"
+                    onClick={() => showMobilemenu()}
+                >
+                    <i className="bi bi-list"></i>
+                </Button>
+            </div>
+            <div className="hstack gap-2">
+                <Button
+                    color="primary"
+                    size="sm"
+                    className="d-sm-block d-md-none"
+                    onClick={Handletoggle}
+                >
+                    {isOpen ? (
+                        <i className="bi bi-x"></i>
+                    ) : (
+                        <i className="bi bi-three-dots-vertical"></i>
+                    )}
+                </Button>
+            </div>
 
-      <Collapse navbar isOpen={isOpen}>
-        <Nav className="me-auto" navbar>
-          <NavItem>
-            <Link to="/starter" className="nav-link">
-              Job Listings
-            </Link>
-          </NavItem>
-          {/* <NavItem>
+            <Collapse navbar isOpen={isOpen}>
+                <Nav className="me-auto" navbar>
+                    <NavItem>
+                        <Link
+                            to="/starter"
+                            className="nav-link"
+                            style={{ color: "white" }}
+                        >
+                            Job Listings
+                        </Link>
+                    </NavItem>
+                    {/* <NavItem>
             <Link to="/about" className="nav-link">
               About
             </Link>
           </NavItem> */}
-          {/* <UncontrolledDropdown inNavbar nav>
+                    {/* <UncontrolledDropdown inNavbar nav>
             <DropdownToggle caret nav>
               DD Menu
             </DropdownToggle>
@@ -84,8 +88,8 @@ const Header = () => {
               <DropdownItem>Reset</DropdownItem>
             </DropdownMenu>
           </UncontrolledDropdown> */}
-        </Nav>
-        {/* <Dropdown isOpen={dropdownOpen} toggle={toggle}>
+                </Nav>
+                {/* <Dropdown isOpen={dropdownOpen} toggle={toggle}>
           <DropdownToggle color="transparent">
             <img
               src={user5}
@@ -104,9 +108,9 @@ const Header = () => {
             <DropdownItem>Logout</DropdownItem>
           </DropdownMenu>
         </Dropdown> */}
-      </Collapse>
-    </Navbar>
-  );
+            </Collapse>
+        </Navbar>
+    );
 };
 
 export default Header;
